c741680192dd610112f25d330ed91373d7b4a496
Jossia Elang <81104672+nexonmegaxus@users.noreply.github.com>
2024-06-20 14:36:00 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        fontFamily: {
+            sans: ['Poppins'],
+            display: ['Poppins'],
+            body: ['Poppins'],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
